71ecd0895c06ceeb74ff83d3cc0d8ecd29384ebc
Renan Lessa <renanlessa@gmail.com>
2024-06-14 16:22:17 -0300

    initial commit

---
+++ src/main/java/com/paneladev/hello/HelloDockerApplication.java
@@ -0,0 +1,13 @@
+package com.paneladev.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloDockerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/paneladev/hello/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.paneladev.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping
+    public String HelloWorld() {
+        return "Hello World!";
+    }
+
+}
